519a18ed2fb9c50a645b3668e35e3388b5fcd056
Rinta01 <rinat.ganbarov@gmail.com>
2020-03-15 02:18:03 +0300

    Updates

---
+++ precache-manifest.e54393effbe3fedf91194086117d3474.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb1827858c84e20c00f3",
+    "url": "/ReactTable/static/js/runtime~main.cb182785.js"
+  },
+  {
+    "revision": "33d3373b7693c8fc2b1f",
+    "url": "/ReactTable/static/js/main.33d3373b.chunk.js"
+  },
+  {
+    "revision": "a7a2e34b4eec3005856d",
+    "url": "/ReactTable/static/js/1.a7a2e34b.chunk.js"
+  },
+  {
+    "revision": "33d3373b7693c8fc2b1f",
+    "url": "/ReactTable/static/css/main.727abfc9.chunk.css"
+  },
+  {
+    "revision": "a7a2e34b4eec3005856d",
+    "url": "/ReactTable/static/css/1.5b982b9c.chunk.css"
+  },
+  {
+    "revision": "c08313ee092c031eeed78d3cbaee59da",
+    "url": "/ReactTable/index.html"
+  }
+];
